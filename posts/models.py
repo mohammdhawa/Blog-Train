34f8f331dc081decc0941848046b0d5a3776927d
Muhammed Recep Hava <66217824+mohammdhawa@users.noreply.github.com>
2024-10-11 00:26:33 +0300

    add category & author models

--- posts/models.py
+++ posts/models.py
@@ -10,6 +10,22 @@ class Post(models.Model):
     publish_date = models.DateTimeField(default=timezone.now())
     tags = TaggableManager()
     image = models.ImageField(upload_to='posts/')
+    author = models.ForeignKey('Author', related_name='author_post', on_delete=models.CASCADE)
+    category = models.ForeignKey('Category', related_name='category_post', on_delete=models.CASCADE)
     
     def __str__(self):
         return self.title[:50]
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=150)
+    
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
