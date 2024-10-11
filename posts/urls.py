c17758e5ebd0dc92f3cef69fd1a9bad45a1c9b30
Muhammed Recep Hava <66217824+mohammdhawa@users.noreply.github.com>
2024-10-12 00:47:37 +0300

    add commenting feature

--- posts/urls.py
+++ posts/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, CreatePost, EditPost, DeletePost
+from .views import PostListView, post_detail, CreatePost, EditPost, DeletePost
 
 
 urlpatterns = [
     path('', PostListView.as_view(), name='posts'),
-    path('post-detail/<int:pk>', PostDetailView.as_view(), name='post-detail'),
+    path('post-detail/<int:pk>', post_detail, name='post-detail'),
     path('posts/new', CreatePost.as_view(), name='create-post'),
     path('posts/edit/<int:pk>', EditPost.as_view(), name='edit-post'),
     path('posts/delete/<int:pk>', DeletePost.as_view(), name='delete-post'),
--- posts/views.py
+++ posts/views.py
@@ -1,16 +1,30 @@
 from django.shortcuts import render, redirect
-from .models import Post
+from .models import Post, Comment
 from django.views.generic import (ListView, DetailView, CreateView, 
                                   UpdateView, DeleteView)
-# from .forms import PostForm
+from .forms import CommentForm
 
 
 class PostListView(ListView):
     model = Post
 
 
-class PostDetailView(DetailView):
-    model = Post
+def post_detail(request, pk):
+    post = Post.objects.get(id=pk)
+    comments = Comment.objects.filter(post=post)
+    
+    form = CommentForm()
+    
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            myform = form.save(commit=False)
+            myform.user = request.user
+            myform.post = post
+            myform.save()
+            return redirect(f"/post-detail/{pk}")
+    
+    return render(request, 'posts/post_detail.html', {'object': post, 'comments': comments, 'form': form})
     
     
 class CreatePost(CreateView):
