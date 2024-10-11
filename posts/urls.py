c643b7ca3b0679c28d7f134ad9d41abb9a36f05b
Muhammed Recep Hava <66217824+mohammdhawa@users.noreply.github.com>
2024-10-11 21:06:32 +0300

    add create & edit & delete posts functionality using FBVs

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import PostListView, PostDetailView
+from .views import PostListView, PostDetailView, create_post, edit_post, delete_post
 
 
 urlpatterns = [
     path('', PostListView.as_view(), name='posts'),
     path('post-detail/<int:pk>', PostDetailView.as_view(), name='post-detail'),
+    path('posts/new', create_post, name='create-post'),
+    path('posts/edit/<int:pk>', edit_post, name='edit-post'),
+    path('posts/delete/<int:pk>', delete_post, name='delete-post'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
 from django.views.generic import ListView, DetailView
+from .forms import PostForm
 
 
 class PostListView(ListView):
@@ -9,3 +10,41 @@ class PostListView(ListView):
 
 class PostDetailView(DetailView):
     model = Post
+    
+    
+def create_post(request):
+    form = PostForm()
+    
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            myform = form.save(commit=False)
+            myform.author = request.user
+            myform.save()
+            return redirect('/')
+    
+    return render(request, 'posts/new.html', {'form': form})
+
+
+def edit_post(request, pk):
+    post = Post.objects.get(id=pk)
+    
+    form = PostForm(instance=post)
+    
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            myform = form.save(commit=False)
+            myform.author = request.user
+            myform.save()
+            return redirect(f"/post-detail/{pk}")
+
+    return render(request, 'posts/new.html', {'form': form})
+
+
+def delete_post(request, pk):
+    post = Post.objects.get(id=pk)
+    
+    post.delete()
+    
+    return redirect('/')
