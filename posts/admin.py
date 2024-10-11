ba40e0287ad267e514b52dc530cff8cba25c554a
Muhammed Recep Hava <66217824+mohammdhawa@users.noreply.github.com>
2024-10-11 18:22:48 +0300

    add comment to admin page

--- posts/admin.py
+++ posts/admin.py
@@ -1,15 +1,25 @@
 from django.contrib import admin
-from .models import Post, Category
+from .models import Post, Category, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
+
+class CommentInline(admin.TabularInline):
+    model = Comment
 
 
 class PostAdmin(SummernoteModelAdmin):
     list_display = ['title', 'id']
+    list_filter = ['category', 'tags', 'draft']
     summernote_fields = ['content']
+    
+    inlines = [CommentInline]
+        
+    
     
 
 
 admin.site.register(Post, PostAdmin)
 
 admin.site.register(Category)
+
+# admin.site.register(Comment)
