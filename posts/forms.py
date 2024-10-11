1f1af696e5d2b0f4f8767cd3b63759325e5fbede
Muhammed Recep Hava <66217824+mohammdhawa@users.noreply.github.com>
2024-10-11 21:19:58 +0300

    add CRUD operations to the Posts using CVB

--- posts/forms.py
+++ posts/forms.py
@@ -2,8 +2,8 @@ from django import forms
 from .models import Post, Category
 
 
-class PostForm(forms.ModelForm):
-    class Meta:
-        model = Post
-        fields = "__all__"
-        exclude = ('author', )
+# class PostForm(forms.ModelForm):
+#     class Meta:
+#         model = Post
+#         fields = "__all__"
+#         exclude = ('author', )
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from .models import Post
-from django.views.generic import ListView, DetailView
-from .forms import PostForm
+from django.views.generic import (ListView, DetailView, CreateView, 
+                                  UpdateView, DeleteView)
+# from .forms import PostForm
 
 
 class PostListView(ListView):
@@ -12,39 +13,20 @@ class PostDetailView(DetailView):
     model = Post
     
     
-def create_post(request):
-    form = PostForm()
-    
-    if request.method == 'POST':
-        form = PostForm(request.POST, request.FILES)
-        if form.is_valid():
-            myform = form.save(commit=False)
-            myform.author = request.user
-            myform.save()
-            return redirect('/')
-    
-    return render(request, 'posts/new.html', {'form': form})
+class CreatePost(CreateView):
+    model = Post
+    template_name = 'posts/new.html'
+    fields = '__all__'
+    success_url = '/'
 
 
-def edit_post(request, pk):
-    post = Post.objects.get(id=pk)
-    
-    form = PostForm(instance=post)
-    
-    if request.method == 'POST':
-        form = PostForm(request.POST, request.FILES, instance=post)
-        if form.is_valid():
-            myform = form.save(commit=False)
-            myform.author = request.user
-            myform.save()
-            return redirect(f"/post-detail/{pk}")
-
-    return render(request, 'posts/new.html', {'form': form})
+class EditPost(UpdateView):
+    model = Post
+    template_name = 'posts/new.html'
+    fields = '__all__'
+    success_url = '/'
 
 
-def delete_post(request, pk):
-    post = Post.objects.get(id=pk)
-    
-    post.delete()
-    
-    return redirect('/')
+class DeletePost(DeleteView):
+    model = Post
+    success_url = '/'
