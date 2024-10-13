29de2d0e06a51868226d4f17c52514693bca2b2f
Muhammed Recep Hava <66217824+mohammdhawa@users.noreply.github.com>
2024-10-13 15:50:33 +0300

    show relation as nested object

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,12 +1,18 @@
 # Instead of form (it converts data to JSON)
 from rest_framework import serializers
 from .models import Post
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = serializers.StringRelatedField()
+    author = UserSerializer()
     category = serializers.StringRelatedField()
     class Meta:
         model = Post
         fields = '__all__'
-    
